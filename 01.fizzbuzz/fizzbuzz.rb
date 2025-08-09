45f4115ff03ea8d397f2dc55999d70c54abf5dd7
nswdry <marin.taniguchi25@gmail.com>
2025-08-09 22:55:19 +0900

    [add]1から20までの数をプリントするプログラムを追加
    3の倍数のときは数の代わりに｢Fizz｣と、5の倍数のときは｢Buzz｣とプリントし、3と5両方の倍数の場合には｢FizzBuzz｣とプリントする

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+for i in 1..20 do
+  if i % 5 == 0 && i % 3 == 0
+    puts "FizzBuzz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  else
+    puts i
+  end
+end
+
