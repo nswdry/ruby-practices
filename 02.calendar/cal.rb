0550708873cfc4968e1dcb19dcb7a3eecc3be24a
nswdry <marin.taniguchi25@gmail.com>
2025-08-15 13:30:02 +0900

    [add]カレンダー機能を追加
    今月のカレンダーを表示するプログラムで、-mで月を、-yで年を指定できる仕様です。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,3 +1,31 @@
 #!/usr/bin/env ruby
 
+require 'date'
+require 'optparse'
 
+options = {}
+opt = OptionParser.new
+
+opt.on('-y YEAR', Integer) {|year| options[:year] = year}
+opt.on('-m MONTH', Integer) {|month| options[:month] = month}
+
+opt.parse!(ARGV)
+
+year = options[:year] || Date.today.year
+month = options[:month] || Date.today.month
+
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+
+puts "      #{month}月 #{year}"
+puts "日 月 火 水 木 金 土"
+print "   " * first_date.wday
+
+(first_date..last_date).each do |date|
+  printf("%2d", date.day)
+  if date.wday == 6 || date.day == last_date.day
+    print "\n"
+  else
+    print " "
+  end
+end
