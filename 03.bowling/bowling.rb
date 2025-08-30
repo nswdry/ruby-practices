1868856f4a6e929940f1212c6ae3add909c195a7
nswdry <marin.taniguchi25@gmail.com>
2025-08-30 23:18:51 +0900

    [fix]sumメソッドを使ってコードをより簡潔にしました
    合計スコアの計算をeachからsumに変更することでより簡潔になりました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -5,23 +5,21 @@ score = ARGV[0]
 shots = score.split(',').flat_map { |s| s == 'X' ? 10 : s.to_i }
 
 i = 0
-point = 0
 
-(1..10).each do |frame|
-  if frame < 10
-    if shots[i] == 10 # 　strike
-      point += 10 + shots[i + 1] + shots[i + 2]
-      i += 1
-    elsif shots[i] + shots[i + 1] == 10 # 　spare
-      point += 10 + shots[i + 2]
-      i += 2
-    else
-      point += shots[i] + shots[i + 1]
-      i += 2
-    end
+point = (1..9).sum do
+  if shots[i] == 10 # 　strike
+    flame_score = 10 + shots[i + 1] + shots[i + 2]
+    i += 1
+  elsif shots[i] + shots[i + 1] == 10 # 　spare
+    flame_score = 10 + shots[i + 2]
+    i += 2
   else
-    point += shots[i..].sum # 10フレーム目
+    flame_score = shots[i] + shots[i + 1]
+    i += 2
   end
+  flame_score
 end
+
+point += shots[i..].sum # 10フレーム目
 
 puts point
