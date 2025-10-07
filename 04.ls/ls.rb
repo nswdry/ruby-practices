91ad41b1815bf3d160e371c23e111d7a80eb717f
nswdry <marin.taniguchi25@gmail.com>
2025-10-07 22:27:11 +0900

    [add]calc_columnsメソッドを追加
    列の数を計算する機能

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,3 +2,12 @@ def fetch_dir_contents
   Dir.glob("*")
 end
 
+def calc_columns(width, max_length)
+  if width < 2 * (max_length + 1)
+    1
+  elsif width < 3 * (max_length + 1)
+    2
+  else
+    3
+  end
+end
