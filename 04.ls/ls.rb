a79f72f1432cef629af7a45a929f525948445a78
nswdry <marin.taniguchi25@gmail.com>
2025-10-16 22:24:58 +0900

    [add]コマンドライン引数-aを受け取った場合、隠しファイルも含めて表示するように変更
    -aオプションがあれば、隠しファイルを表示し、なければ表示しないよう機能を追加しました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,17 +2,28 @@
 # frozen_string_literal: true
 
 require 'io/console'
+require 'optparse'
+opt = OptionParser.new
 
-def main
-  files = fetch_dir_contents
+params = {}
+
+opt.on('-a') { params[:a] = true }
+opt.parse(ARGV)
+
+def main(params)
+  files = fetch_dir_contents(params)
   width = IO.console.winsize[1]
   max_length = files.map(&:length).max
   cols = calc_columns(width, max_length)
   display_in_columns(files, cols, max_length)
 end
 
-def fetch_dir_contents
-  Dir.glob('*')
+def fetch_dir_contents(params)
+  if params[:a]
+    Dir.entries('.').sort
+  else
+    Dir.glob('*')
+  end
 end
 
 def calc_columns(width, max_length)
@@ -28,4 +39,4 @@ def display_in_columns(files, cols, max_length)
   end
 end
 
-main
+main(params)
