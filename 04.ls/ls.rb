e428bb46647eb800dbd7a5b8b28920a3d780aa44
nswdry <marin.taniguchi25@gmail.com>
2025-12-05 23:27:17 +0900

    [change] long表示処理をメソッドに分割しました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -47,56 +47,59 @@ end
 def display_long(files)
   files.each do |file|
     st = File.stat(file)
-
-    symbols = {
-      'file' => '-',
-      'directory' => 'd',
-      'characterSpecial' => 'c',
-      'blockSpecial' => 'b',
-      'fifo' => 'p',
-      'link' => 'l',
-      'socket' => 's'
-    }
-
-    file_type = symbols.fetch(st.ftype, '?')
-    link = st.nlink
-    user_name = Etc.getpwuid(st.uid).name
-    group_name = Etc.getgrgid(st.gid).name
-    size = st.size
-
-    last_updated =
-      if st.mtime.to_date >= Date.today << 6
-        st.mtime.strftime('%_m %_d %H:%M')
-      else
-        st.mtime.strftime('%_m %_d %Y')
-      end
-
-    permissions = [
-      { r: 0o400, w: 0o200, x: 0o100, special: 0o4000, exec: %w[s S] },
-      { r: 0o040, w: 0o020, x: 0o010, special: 0o2000, exec: %w[s S] },
-      { r: 0o004, w: 0o002, x: 0o001, special: 0o1000, exec: %w[t T] }
-    ]
-
-    mode = permissions.map do
-      r = st.mode & it[:r] != 0 ? 'r' : '-'
-      w = st.mode & it[:w] != 0 ? 'w' : '-'
-      x = st.mode & it[:x] != 0 ? 'x' : '-'
-
-      if st.mode & it[:special] != 0
-        x = x == 'x' ? it[:exec][0] : it[:exec][1]
-      end
-
-      "#{r}#{w}#{x}"
-    end.join
-
-    printf "%s %3d %-2s %-7s %d %11s\n",
-           "#{file_type}#{mode}",
-           link,
-           user_name,
-           group_name,
-           size,
-           last_updated
+    puts format_long_line(file, st)
   end
+end
+
+def format_long_line(file, st)
+  type        = file_type(st.ftype)
+  permission  = format_mode(st)
+  link        = st.nlink
+  user_name   = Etc.getpwuid(st.uid).name
+  group_name  = Etc.getgrgid(st.gid).name
+  size        = st.size
+
+  last_updated =
+    if st.mtime.to_date >= Date.today << 6
+      st.mtime.strftime('%_m %_d %H:%M')
+    else
+      st.mtime.strftime('%_m %_d %Y')
+    end
+
+  format '%s%s %3d %-2s %-7s %d %11s',
+         type, permission, link, user_name, group_name, size, last_updated
+end
+
+def file_type(ftype)
+  {
+    'file' => '-',
+    'directory' => 'd',
+    'characterSpecial' => 'c',
+    'blockSpecial' => 'b',
+    'fifo' => 'p',
+    'link' => 'l',
+    'socket' => 's'
+  }.fetch(ftype, '?')
+end
+
+def format_mode(st)
+  permission_bits = [
+    { r: 0o400, w: 0o200, x: 0o100, special: 0o4000, exec: %w[s S] },
+    { r: 0o040, w: 0o020, x: 0o010, special: 0o2000, exec: %w[s S] },
+    { r: 0o004, w: 0o002, x: 0o001, special: 0o1000, exec: %w[t T] }
+  ]
+
+  permission_bits.map do
+    r = st.mode & it[:r] != 0 ? 'r' : '-'
+    w = st.mode & it[:w] != 0 ? 'w' : '-'
+    x = st.mode & it[:x] != 0 ? 'x' : '-'
+
+    if st.mode & it[:special] != 0
+      x = x == 'x' ? it[:exec][0] : it[:exec][1]
+    end
+
+    "#{r}#{w}#{x}"
+  end.join
 end
 
 main(params)
