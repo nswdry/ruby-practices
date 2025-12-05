2a93e5c1768ebd8fe46813bc01cf58f828f97793
nswdry <marin.taniguchi25@gmail.com>
2025-12-05 23:50:05 +0900

    [change]カラム幅を計算し、ls -lの出力に近づけるよう調整しました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -16,11 +16,12 @@ opt.parse(ARGV)
 
 def main(params)
   files = fetch_dir_contents(params)
+  st = files.to_h { [it, File.lstat(it)] }
   width = IO.console.winsize[1]
   max_length = files.map(&:length).max
   cols = calc_columns(width, max_length)
   if params[:l]
-    display_long(files)
+    display_long(files, st)
   else
     display_in_columns(files, cols, max_length)
   end
@@ -44,20 +45,27 @@ def display_in_columns(files, cols, max_length)
   end
 end
 
-def display_long(files)
-  files.each do |file|
-    st = File.stat(file)
-    puts format_long_line(file, st)
+def display_long(files, st)
+  total_blocks = st.values.sum(&:blocks)
+  puts "total #{total_blocks}"
+
+  link_width = st.values.map { it.nlink.to_s.length }.max
+  user_width = st.values.map { Etc.getpwuid(it.uid).name.length }.max
+  group_width = st.values.map { Etc.getgrgid(it.gid).name.length }.max
+  size_width = st.values.map { it.size.to_s.length }.max
+
+  files.each do
+    puts format_long_line(it, st[it], link_width, user_width, group_width, size_width)
   end
 end
 
-def format_long_line(file, st)
-  type        = file_type(st.ftype)
-  permission  = format_mode(st)
-  link        = st.nlink
-  user_name   = Etc.getpwuid(st.uid).name
-  group_name  = Etc.getgrgid(st.gid).name
-  size        = st.size
+def format_long_line(file, st, link_width, user_width, group_width, size_width)
+  type = file_type(st.ftype)
+  permission = format_mode(st)
+  link = st.nlink
+  user_name  = Etc.getpwuid(st.uid).name
+  group_name = Etc.getgrgid(st.gid).name
+  size = st.size
 
   last_updated =
     if st.mtime.to_date >= Date.today << 6
@@ -66,8 +74,8 @@ def format_long_line(file, st)
       st.mtime.strftime('%_m %_d %Y')
     end
 
-  format '%s%s %3d %-2s %-7s %d %11s',
-         type, permission, link, user_name, group_name, size, last_updated
+  format "%s%s  %-#{link_width}d %#{user_width}s  %#{group_width}s  %#{size_width}d %s %s",
+         type, permission, link, user_name, group_name, size, last_updated, file
 end
 
 def file_type(ftype)
