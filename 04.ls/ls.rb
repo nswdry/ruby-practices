0939b1672268d6ce5927c4d53c28301fb61ae4bf
nswdry <marin.taniguchi25@gmail.com>
2025-12-06 22:18:58 +0900

    [fix]rubocopの違反を修正
    短すぎる変数名の修正と多すぎる引数を減らしました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -16,12 +16,12 @@ opt.parse(ARGV)
 
 def main(params)
   files = fetch_dir_contents(params)
-  st = files.to_h { [it, File.lstat(it)] }
+  stat = files.to_h { [it, File.lstat(it)] }
   width = IO.console.winsize[1]
   max_length = files.map(&:length).max
   cols = calc_columns(width, max_length)
   if params[:l]
-    display_long(files, st)
+    display_long(files, stat)
   else
     display_in_columns(files, cols, max_length)
   end
@@ -45,36 +45,43 @@ def display_in_columns(files, cols, max_length)
   end
 end
 
-def display_long(files, st)
-  total_blocks = st.values.sum(&:blocks)
+def display_long(files, stat)
+  total_blocks = stat.values.sum(&:blocks)
   puts "total #{total_blocks}"
 
-  link_width = st.values.map { it.nlink.to_s.length }.max
-  user_width = st.values.map { Etc.getpwuid(it.uid).name.length }.max
-  group_width = st.values.map { Etc.getgrgid(it.gid).name.length }.max
-  size_width = st.values.map { it.size.to_s.length }.max
+  link_width = stat.values.map { it.nlink.to_s.length }.max
+  user_width = stat.values.map { Etc.getpwuid(it.uid).name.length }.max
+  group_width = stat.values.map { Etc.getgrgid(it.gid).name.length }.max
+  size_width = stat.values.map { it.size.to_s.length }.max
+
+  field_widths = {
+    link: link_width,
+    user: user_width,
+    group: group_width,
+    size: size_width
+  }
 
   files.each do
-    puts format_long_line(it, st[it], link_width, user_width, group_width, size_width)
+    puts format_long_line(it, stat[it], field_widths)
   end
 end
 
-def format_long_line(file, st, link_width, user_width, group_width, size_width)
-  type = file_type(st.ftype)
-  permission = format_mode(st)
-  link = st.nlink
-  user_name  = Etc.getpwuid(st.uid).name
-  group_name = Etc.getgrgid(st.gid).name
-  size = st.size
+def format_long_line(file, stat, field_widths)
+  type = file_type(stat.ftype)
+  permission = format_mode(stat)
+  link = stat.nlink
+  user_name  = Etc.getpwuid(stat.uid).name
+  group_name = Etc.getgrgid(stat.gid).name
+  size = stat.size
 
   last_updated =
-    if st.mtime.to_date >= Date.today << 6
-      st.mtime.strftime('%_m %_d %H:%M')
+    if stat.mtime.to_date >= Date.today << 6
+      stat.mtime.strftime('%_m %_d %H:%M')
     else
-      st.mtime.strftime('%_m %_d %Y')
+      stat.mtime.strftime('%_m %_d %Y')
     end
 
-  format "%s%s  %-#{link_width}d %#{user_width}s  %#{group_width}s  %#{size_width}d %s %s",
+  format "%s%s  %-#{field_widths[:link]}d %#{field_widths[:user]}s  %#{field_widths[:group]}s  %#{field_widths[:size]}d %s %s",
          type, permission, link, user_name, group_name, size, last_updated, file
 end
 
@@ -90,7 +97,7 @@ def file_type(ftype)
   }.fetch(ftype, '?')
 end
 
-def format_mode(st)
+def format_mode(stat)
   permission_bits = [
     { r: 0o400, w: 0o200, x: 0o100, special: 0o4000, exec: %w[s S] },
     { r: 0o040, w: 0o020, x: 0o010, special: 0o2000, exec: %w[s S] },
@@ -98,11 +105,11 @@ def format_mode(st)
   ]
 
   permission_bits.map do
-    r = st.mode & it[:r] != 0 ? 'r' : '-'
-    w = st.mode & it[:w] != 0 ? 'w' : '-'
-    x = st.mode & it[:x] != 0 ? 'x' : '-'
+    r = stat.mode & it[:r] != 0 ? 'r' : '-'
+    w = stat.mode & it[:w] != 0 ? 'w' : '-'
+    x = stat.mode & it[:x] != 0 ? 'x' : '-'
 
-    if st.mode & it[:special] != 0
+    if stat.mode & it[:special] != 0
       x = x == 'x' ? it[:exec][0] : it[:exec][1]
     end
 
